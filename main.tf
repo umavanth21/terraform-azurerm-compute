d79b86726dae72cb878d2857a8208577a47a59a4
umavanth21 <56533761+umavanth21@users.noreply.github.com>
2020-04-15 21:16:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+provider "azurerm" {
+  version = "~> 1.44"
+ subscription_id = var.subscription_id
+   client_id       = var.client_id
+   client_secret   = var.client_secret
+   tenant_id       = var. tenant_id
+ }
 module "os" {
   source       = "./os"
   vm_os_simple = var.vm_os_simple
